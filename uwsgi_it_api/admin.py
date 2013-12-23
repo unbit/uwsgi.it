2d75cfc07409fdfe4ceaa64a551703403956c842
Unbit <info@unbit.it>
2013-12-23 09:42:42 +0100

    fixed admin.py

--- uwsgi_it_api/admin.py
+++ uwsgi_it_api/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from containers.models import *
+from uwsgi_it_api.models import *
 
 class ServerAdmin(admin.ModelAdmin):
     def memory_status(self):
