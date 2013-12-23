3c6a9221b3fc586eed2b3c46212fdd32d4b9270d
Unbit <info@unbit.it>
2013-12-23 12:42:46 +0100

    another fix for urls.py

--- uwsgi_it_api/urls.py
+++ uwsgi_it_api/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns
 
 urlpatterns = patterns('uwsgi_it_api.views',
     (r'^containers/$', 'containers'),
-    (r'^containers(\d+)$', 'container'),
-    (r'^containers(\d+)\.ini$', 'container_ini'),
+    (r'^containers/(\d+)$', 'container'),
+    (r'^containers/(\d+)\.ini$', 'container_ini'),
 )
