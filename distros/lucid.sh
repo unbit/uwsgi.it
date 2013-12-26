6e55a522030633180233bcb111e8c698b11a812b
Unbit <info@unbit.it>
2013-12-26 19:38:58 +0100

    mount rootfs ro

--- distros/lucid.sh
+++ distros/lucid.sh
@@ -1,1 +1,1 @@
-debootstrap --components=main,universe,multiverse --include=build-essential,git-core,redis-server,lua5.1,postgresql,libpq-dev,python-dev,memcached,mongodb,libperl-dev,ruby lucid /distros/lucid
+debootstrap --components=main,universe,multiverse --include=build-essential,git-core,redis-server,lua5.1,postgresql,libpq-dev,python-dev,memcached,mongodb,libperl-dev,ruby,wget lucid /distros/lucid
