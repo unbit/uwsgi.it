6abbae22e38826c86e1f8220d5d82d41fbb91d86
Unbit <info@unbit.it>
2014-01-11 19:14:03 +0100

    improved firewall

--- firewall.sh
+++ firewall.sh
@@ -1,6 +1,10 @@
 LEGION_NODES=/etc/uwsgi/legion_nodes
 NODES=/etc/uwsgi/nodes
 
+if [ ! -f $NODES ]; then
+exit 0
+fi
+
 # tuntap firewall
 iptables -F tuntap
 while read line
@@ -8,6 +12,10 @@ do
 	iptables -A tuntap -s $line -p udp --sport 999 -j ACCEPT
 done < $NODES
 iptables -A tuntap -j DROP
+
+if [ ! -f $LEGION_NODES ]; then
+exit 0
+fi
 
 #while read line
 #do
