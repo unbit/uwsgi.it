464a56930c384b8ce4c9ab9fe14823708f7531cb
Unbit <info@unbit.it>
2015-02-05 09:59:44 +0100

    added busybox builders

---
+++ distros/busybox.sh
@@ -0,0 +1,27 @@
+set -u
+set -e
+wget -O busybox-x86_64 http://www.busybox.net/downloads/binaries/latest/busybox-x86_64
+mkdir -p /distros/busybox/bin
+cp busybox-x86_64 /distros/busybox/bin/busybox
+chmod 755 /distros/busybox/bin/busybox
+mkdir /distros/busybox/.old_root
+mkdir /distros/busybox/containers
+mkdir -p /distros/busybox/etc/security
+touch /distros/busybox/etc/security/pam_env.conf
+touch /distros/busybox/etc/security/limits.conf
+mkdir /distros/busybox/proc
+mkdir /distros/busybox/run
+mkdir /distros/busybox/tmp
+mkdir /distros/busybox/dev
+mkdir -p /distros/busybox/usr/local
+mkdir /distros/busybox/var
+mkdir /distros/busybox/var/log
+mkdir /distros/busybox/var/spool
+mkdir /distros/busybox/var/tmp
+
+chroot /distros/busybox /bin/busybox sh <<'EOF'
+/bin/busybox --install -s /bin
+echo "#!/bin/sh" > /bin/bash
+echo "/bin/ash $@" >> /bin/bash
+chmod 755 /bin/bash
+EOF
---
+++ distros/busybox32.sh
@@ -0,0 +1,27 @@
+set -u
+set -e
+wget -O busybox-i686 http://www.busybox.net/downloads/binaries/latest/busybox-i686
+mkdir -p /distros/busybox32/bin
+cp busybox-i686 /distros/busybox32/bin/busybox
+chmod 755 /distros/busybox32/bin/busybox
+mkdir /distros/busybox32/.old_root
+mkdir /distros/busybox32/containers
+mkdir -p /distros/busybox32/etc/security
+touch /distros/busybox32/etc/security/pam_env.conf
+touch /distros/busybox32/etc/security/limits.conf
+mkdir /distros/busybox32/proc
+mkdir /distros/busybox32/run
+mkdir /distros/busybox32/tmp
+mkdir /distros/busybox32/dev
+mkdir -p /distros/busybox32/usr/local
+mkdir /distros/busybox32/var
+mkdir /distros/busybox32/var/log
+mkdir /distros/busybox32/var/spool
+mkdir /distros/busybox32/var/tmp
+
+chroot /distros/busybox32 /bin/busybox sh <<'EOF'
+/bin/busybox --install -s /bin
+echo "#!/bin/sh" > /bin/bash
+echo "/bin/ash $@" >> /bin/bash
+chmod 755 /bin/bash
+EOF
